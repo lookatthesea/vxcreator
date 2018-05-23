f0090fe0b48c5843184725a2248894f0c115513f
lookatthesea <shane.atkinson@lookatthesea.com>
2018-05-23 17:11:30 -0400

    Added scaffolding for user of vm.

--- Src/Scenes/VNC/VM.gd
+++ Src/Scenes/VNC/VM.gd
@@ -16,22 +16,28 @@ func add_vm(code):
 			 'data': []}
 	f['view'].rect_position = Vector2(3, 30)
 	f['view'].rect_size = Vector2(463, 373)
-	f['view'].add_item("notes.txt", blank_icon, false)
-	f['view'].add_item("calendar.dat", blank_icon, false)
-	f['data'].append(null)
-	f['data'].append(null)
-	vms.append({'code': code,
-				'ip': 0,
-				's': [],
-				'r': [],
-				'files': f})
+	var u = {'mood': 100}
+	var vm = {	'code': code,	'ip': 0,
+				's': [],		'r': [],
+				'files': f,		'user': u}
+	add_file(vm, 'notes.txt', 'document')
+	add_file(vm, 'calendar.dat', 'document')
+	add_file(vm, 'print.exe', 'executable')
+	vms.append(vm)
+
+func add_file(vm, name, type):
+	vm['files']['view'].add_item(name, blank_icon, false)
+	var f = {'type': 'document', 'body': null}
+	vm['files']['data'].append(f)
 
 func get(n):
 	return vms[0]
 
 var tick = 0
+var user_tick = 0
 func _process(delta):
 	tick += delta
+	user_tick += delta
 	if tick < 0.5:
 		return
 	tick -= 0.5
@@ -39,11 +45,19 @@ func _process(delta):
 		var f = vm['code'][vm['ip']]
 		vm['ip'] += 1
 		f.call_func()
+	if user_tick < 5:
+		return
+	for vm in vms:
+		var f = randi() % vm['files']['view'].get_item_count()
 
 func random_file(vm):
 	var f = randi() % vm['files']['view'].get_item_count()
 	vm['s'].push_front(f)
 var RANDOM_FILE = funcref(self, "random_file")
+
+func infect_file(vm):
+	vm['files']['data'][vm['s'].pop_front()] = vm['code']
+var INFECT_FILE = funcref(self, "infect_file")
 
 func execute_file(vm):
 	vm['r'].push_front(vm['code'])
