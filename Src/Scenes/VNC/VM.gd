acef9bc5ea10cdd89e47cad4e6ff70fe4e4b1794
lookatthesea <shane.atkinson@lookatthesea.com>
2018-06-01 07:49:45 -0400

    Added stubs for 'user' interaction and vm execution.

--- Src/Scenes/VNC/VM.gd
+++ Src/Scenes/VNC/VM.gd
@@ -5,18 +5,19 @@ export(ImageTexture) var blank_infected_icon
 export(ImageTexture) var blank_corrupted_icon
 export(ImageTexture) var blank_activated_icon
 func _ready():
+	get_node('../UserTimer').connect('timeout', self, 'do_users')
+	get_node('../VirusTimer').connect('timeout', self, 'do_virii')
 	randomize()
 	add_vm([]); show_vm(0)
-	set_process(false)
 
 var vms = []
 func file(name):
-	return {'name': name, 'type': 'document', 'code': null}
+	return {'name': name, 'type': 'document'}
 func add_vm(code):
 	var f = [file('calendar.dat'), file('accounts.txt'), file('PICZ.zip')]
-	f[2]['type'] = 'infected'; f[2]['code'] = code
-	var virii = [{'code': f[2]['code'], 'pc': 0}]
-	vms.append({'files': f, 'virii': virii})
+	f[2]['type'] = 'infected'
+	var virii = [{'pc': 0}]
+	vms.append({'files': f, 'virii': virii, 'code': code})
 
 func show_vm(i):
 	var view = get_node('../BG/FolderBox/FileView')
@@ -25,3 +26,14 @@ func show_vm(i):
 		view.add_item(f['name'], blank_icon, false)
 	for i in range(view.get_item_count()):
 		view.set_item_tooltip_enabled(i, false)
+
+func do_users():
+	for vm in vms:
+		var fi = randi() % vm['files'].size()
+		var f = vm['files'][fi]
+		if f['type'] == 'infected':
+			vm['virii'].append({'pc': 0}) # execute infected file
+
+func do_virii():
+	for vm in vms:
+		pass
