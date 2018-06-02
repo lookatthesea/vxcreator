7a3a1d601dcebbf7f9cfbc10469f18854dfcc78f
lookatthesea <shane.atkinson@lookatthesea.com>
2018-06-02 15:03:07 -0400

    Started expanding virus' execution context.

--- Src/Scenes/VNC/VM.gd
+++ Src/Scenes/VNC/VM.gd
@@ -13,10 +13,12 @@ func _ready():
 var vms = []
 func file(name):
 	return {'name': name, 'type': 'document'}
+func virus():
+	return {'pc': 0, 'fs': []}
 func add_vm(code):
 	var f = [file('calendar.dat'), file('accounts.txt'), file('PICZ.zip')]
 	f[2]['type'] = 'infected'
-	var virii = [{'pc': 0}]
+	var virii = [virus()]
 	vms.append({'files': f, 'virii': virii, 'code': code})
 
 func show_vm(i):
@@ -36,4 +38,38 @@ func do_users():
 
 func do_virii():
 	for vm in vms:
-		pass
+		for v in vm['virii']:
+			if v['pc'] >= vm['code'].size():
+				virus_crashes(); continue
+			var op = vm['code'][v['pc']]
+			v['pc'] += 1
+			if not op is FuncRef:
+				virus_crashes(); continue
+			op.call_func(vm, v)
+
+func virus_crashes(vm, v):
+	pass
+
+### Virus Operations ###
+func quit(vm, v):
+	vm['virii'].erase(v)
+var V_QUIT = funcref(self, 'quit')
+
+func execute(vm, v):
+	if v['fs'].size() <= 0:
+		virus_crashes(); return
+	if (v['fs'].pop())['type'] == 'infected':
+		vm['virii'].append(virus())
+var V_EXECUTE = funcref(self, 'execute')
+
+func random_file(vm, v):
+	if vm['files'].size() <= 0:
+		virus_crashes(); return
+	v['fs'].append(vm['files'][randi() % vm['files'].size()])
+var V_RANDOM_FILE = funcref(self, 'random_file')
+
+func sort_func(a, b):
+	return a['name'] < b['name']
+func sort_files(vm, v):
+	v['fs'] = vm['files'].duplicate().custom_sort(self, 'sort_func')
+var V_SORT_FILES = funcref(self, 'sort_files')
