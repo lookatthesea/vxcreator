1c2d8e0d908ed78c932df17fcf70376280250d59
lookatthesea <shane.atkinson@lookatthesea.com>
2018-06-12 10:55:39 -0400

    Reworked VM display. PC now overflows. Virus operations tested.

--- Src/Scenes/VNC/VM.gd
+++ Src/Scenes/VNC/VM.gd
@@ -5,71 +5,70 @@ export(ImageTexture) var blank_infected_icon
 export(ImageTexture) var blank_corrupted_icon
 export(ImageTexture) var blank_activated_icon
 func _ready():
-	get_node('../UserTimer').connect('timeout', self, 'do_users')
-	get_node('../VirusTimer').connect('timeout', self, 'do_virii')
 	randomize()
-	add_vm([]); show_vm(0)
+	add_vm([V_TEST, V_RANDOM_FILE, V_TEST, V_INFECT, V_TEST, V_EXECUTE, V_QUIT])
+	get_node('../VirusTimer').connect('timeout', self, 'do_virii')
+	get_node('../VirusTimer').start()
+	get_node('../DisplayTimer').connect('timeout', self, 'show_vm')
+	get_node('../DisplayTimer').start()
 
 var vms = []
 func file(name):
-	return {'name': name, 'type': 'document'}
+	return {'name': name, 'type': 'document', 'icon': blank_icon}
 func virus():
-	return {'pc': 0, 'fs': []}
+	return {'pc': 0, 'file': -1}
 func add_vm(code):
 	var f = [file('calendar.dat'), file('accounts.txt'), file('PICZ.zip')]
 	f[2]['type'] = 'infected'
+	f[2]['icon'] = blank_infected_icon
 	var virii = [virus()]
 	vms.append({'files': f, 'virii': virii, 'code': code})
 
-func show_vm(i):
+var viewing = 0
+func show_vm():
 	var view = get_node('../BG/FolderBox/FileView')
 	view.clear()
-	for f in vms[i]['files']:
-		view.add_item(f['name'], blank_icon, false)
+	for f in vms[viewing]['files']:
+		view.add_item(f['name'], f['icon'], false)
 	for i in range(view.get_item_count()):
 		view.set_item_tooltip_enabled(i, false)
-
-func do_users():
-	for vm in vms:
-		var fi = randi() % vm['files'].size()
-		var f = vm['files'][fi]
-		if f['type'] == 'infected':
-			vm['virii'].append({'pc': 0}) # execute infected file
 
 func do_virii():
 	for vm in vms:
 		for v in vm['virii']:
-			if v['pc'] >= vm['code'].size():
-				virus_crashes(vm, v); continue
 			var op = vm['code'][v['pc']]
-			v['pc'] += 1
+			v['pc'] = (v['pc'] + 1) % vm['code'].size()
 			if not op is FuncRef:
 				virus_crashes(vm, v); continue
 			op.call_func(vm, v)
 
-func virus_crashes(vm, v):
-	pass
+### Virus Opertaions ###
+func test(vm, v):
+	print(vm['virii'])
+var V_TEST = funcref(self, 'test')
 
-### Virus Operations ###
+func random_file(vm, v):
+	if vm['files'].size() <= 0:
+		v['file'] = -1
+		return
+	v['file'] = randi() % vm['files'].size()
+var V_RANDOM_FILE = funcref(self, 'random_file')
+
+func infect(vm, v):
+	if v['file'] < 0:
+		return
+	vm['files'][v['file']]['icon'] = blank_infected_icon
+	vm['files'][v['file']]['type'] = 'infected'
+var V_INFECT = funcref(self, 'infect')
+
 func quit(vm, v):
 	vm['virii'].erase(v)
 var V_QUIT = funcref(self, 'quit')
 
 func execute(vm, v):
-	if v['fs'].size() <= 0:
-		virus_crashes(vm, v); return
-	if (v['fs'].pop())['type'] == 'infected':
-		vm['virii'].append(virus())
+	if v['file'] < 0:
+		return
+	if vm['files'][v['file']]['type'] != 'infected':
+		return
+	vm['virii'].append(virus())
 var V_EXECUTE = funcref(self, 'execute')
-
-func random_file(vm, v):
-	if vm['files'].size() <= 0:
-		virus_crashes(vm, v); return
-	v['fs'].append(vm['files'][randi() % vm['files'].size()])
-var V_RANDOM_FILE = funcref(self, 'random_file')
-
-func sort_func(a, b):
-	return a['name'] < b['name']
-func sort_files(vm, v):
-	v['fs'] = vm['files'].duplicate().custom_sort(self, 'sort_func')
-var V_SORT_FILES = funcref(self, 'sort_files')
